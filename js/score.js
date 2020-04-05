9809b84bdd52b625ba462d12e7b1285c7cf04468
Jacob <jacob.biederman@mymail.champlain.edu>
2020-04-05 16:44:30 -0400

    added done button to score display

--- js/score.js
+++ js/score.js
@@ -5,19 +5,19 @@ var modal = document.getElementById("score");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 function displayScore(score1, score2){
     modal.style.display = "block";
@@ -28,4 +28,10 @@ function displayScore(score1, score2){
         $("div.score-content").append('<h1 class="score-header">Player 2 Wins!</h1>');
     }
     $("div.score-content").append("<p>Player 1 Scored: " + score1 + " <br />Player 2 Scored: " + score2 + "</p>");
+    $("div.score-content").append('<div style="text-align: center;">'+
+    '<div onClick="startHelper()" class="btn-done">Done</div>'+
+    '</div');
+}
+function startHelper(){
+    location.reload();
 }
